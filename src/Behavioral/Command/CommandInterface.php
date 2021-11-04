4837e6b2d81541b45d22b93cad820820a508a60c
Fabrizio Di Napoli <hyunk3l@gmail.com>
2021-11-04 19:57:48 +0100

    Remove useless comment

--- src/Behavioral/Command/CommandInterface.php
+++ src/Behavioral/Command/CommandInterface.php
@@ -2,10 +2,6 @@
 
 namespace Hyunk3l\PhpDesignPatterns\Behavioral\Command;
 
-/**
- * Interface CommandInterface
- * @package Hyunk3l\PhpDesignPatterns\Behavioral\Command
- */
 interface CommandInterface
 {
     public function execute(): string;
